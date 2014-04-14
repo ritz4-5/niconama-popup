cd09e61328170a692e572fcc2f7165e5255a9eff
ritz4_5 <4.5ritz@gmail.com>
2014-04-14 17:47:20 +0900

    delete addressbar

--- js/popup.js
+++ js/popup.js
@@ -1,27 +1,24 @@
 
-  $(function(){
 
-    /* ポップアップボタンを追加する */
-    $("#watch_title_box .title").append('<input id="popup_button" type="button" value="ポップアップで開く">');
+/* ポップアップボタンを追加する */
+$("#watch_title_box .title").append('<input id="popup_button" type="button" value="ポップアップで開く">');
 
-    $('#popup_button').click(function(){
-        //別ウィンドウで現在URLを開く
-        var currentUrl = location.href;
-        var popupFlag = "popup=true";
-        window.open(currentUrl+popupFlag,"ニコ生ポップアップ","width=960,height=488,scrollbars=no");
+$('#popup_button').click(function(){
+    //別ウィンドウで現在URLを開く
+    var currentUrl = location.href;
+    var popupFlag = "popup=true";
+    chrome.runtime.sendMessage({greeting: "popup", openurl: currentUrl+popupFlag},function(response){
+        if(response.finish) console.log("returned");
+    });
+});
 
+//URLにポップアップフラグが含まれていたらプレイヤーを抜き出す
+if(location.href.indexOf("popup=true") != -1){
+    //スクロールバーを非表示
+    $('html').css({
+        'overflow': 'hidden'
     });
 
-    //URLにポップアップフラグが含まれていたらプレイヤーを抜き出す
-    if(location.href.indexOf("popup=true") != -1){
-        //スクロールバーを非表示
-        $('html').css({
-            'overflow': 'hidden'
-        });
+    $("body").html($("#watch_player_box").html());
 
-        $("body").html($("#watch_player_box").html());
-
-    }
-
-
-  })
+}
