d828078962ec1f36d9eb8c45527d4590eb31a0d0
ritz4_5 <4.5ritz@gmail.com>
2014-04-15 12:23:57 +0900

    bug fix:  multi window can't opend.

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ $(function(){
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
      if(request.greeting == "popup"){
         sendResponse({finish : true});
-        window.open(request.openurl,"ニコ生ポップアップ","width=960,height=488,scrollbars=no");
+        window.open(request.openurl,"","width=960,height=488,scrollbars=no");
 
      }
   });
